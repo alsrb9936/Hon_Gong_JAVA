b64afc62b8093e0d043b0e00648de1f6b7457bc0
PushGyu <ghfn123@gmail.com>
2021-12-17 20:43:33 +0900

    12/17 upload

--- src/sec04/exam04/QStopExample.java
+++ src/sec04/exam04/QStopExample.java
@@ -1,7 +1,9 @@
 package sec04.exam04;
 
+import java.io.IOException;
+
 public class QStopExample {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         int KeyCode;
 
         while(true){
